0b303360dc86feafc288e454b0449d92d9830b21
Baptiste Vincent <baptistevincent@Baptiste-Mac.local>
2026-07-15 12:51:58 +0200

    Add root favicon icon

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const mimeTypes = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".ico": "image/x-icon",
   ".png": "image/png",
   ".svg": "image/svg+xml; charset=utf-8",
   ".webmanifest": "application/manifest+json; charset=utf-8"
